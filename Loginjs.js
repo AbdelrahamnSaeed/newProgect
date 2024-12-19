4ae19c346871393e411979cf5830b1a3722a3ca6
AbdelrahamnSaeed <abdosaeed792@gmail.com>
2024-12-19 17:58:30 +0200

    lastNewApdate

--- Loginjs.js
+++ Loginjs.js
@@ -1,5 +1,4 @@
-function validateForm(e) {
-    e.preventdefault();
+function validateForm() {
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
